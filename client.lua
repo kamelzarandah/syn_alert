0ef4b88ca824036eff9d490e2d25efbd7ba19e74
Blue <62485293+kamelzarandah@users.noreply.github.com>
2021-05-24 15:27:57 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -15,16 +15,18 @@ end
 Citizen.CreateThread(function()
     for k,v in pairs(Config.alerts) do 
         RegisterCommand(v.command, function(source, args, rawCommand)
-            if v.deadcheck then 
-                if IsEntityDead(PlayerPedId()) then
+            if not cooldown then 
+                if v.deadcheck then 
+                    if IsEntityDead(PlayerPedId()) then
+                        v.blip.coords = GetEntityCoords(PlayerPedId())
+                        TriggerServerEvent("syn_alert:sendalert",GetPlayers(),v.jobs,v.msg,v.blip)
+                        cooldown = true
+                    end
+                else
                     v.blip.coords = GetEntityCoords(PlayerPedId())
                     TriggerServerEvent("syn_alert:sendalert",GetPlayers(),v.jobs,v.msg,v.blip)
                     cooldown = true
                 end
-            else
-                v.blip.coords = GetEntityCoords(PlayerPedId())
-                TriggerServerEvent("syn_alert:sendalert",GetPlayers(),v.jobs,v.msg,v.blip)
-                cooldown = true
             end
         end, false)
     end
@@ -40,7 +42,7 @@ Citizen.CreateThread(function()
 	    	end
             if 0 >= timer and cooldown then 
                 cooldown = false 
-                timer = Config.racestartdelay
+                timer = Config.alertcooldown
             end
 	    end
     end
@@ -48,13 +50,13 @@ end)
 
 RegisterNetEvent("syn_alert:createblip")
 AddEventHandler("syn_alert:createblip", function(blip)
-    local blip =  N_0x554d9d53f696d002(1664425300, blip.coords.x, blip.coords.y, blip.coords.z)
-    SetBlipScale(blip, 0.2)
-    SetBlipSprite(blip, blip.blipsprite, 1) 
-    Citizen.InvokeNative(0x9CB1A1623062F402, blip, blip.Name)
-    Citizen.InvokeNative(0x662D364ABF16DE2F,blip,0x6F85C3CE)
+    local blipx =  N_0x554d9d53f696d002(1664425300, blip.coords.x, blip.coords.y, blip.coords.z)
+    SetBlipScale(blipx, 0.2)
+    SetBlipSprite(blipx, blip.blipsprite, 1) 
+    Citizen.InvokeNative(0x9CB1A1623062F402, blipx, blip.Name)
+    Citizen.InvokeNative(0x662D364ABF16DE2F,blipx,0x6F85C3CE)
     table.insert(blips, {
-      blipz = blip
+      blipz = blipx
     })
 end)
 
